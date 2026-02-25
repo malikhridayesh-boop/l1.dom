35fd0035062e56dda871146119ae82999dc88569
Hridayesh Malik <malikhridayesh@gmail.com>
2026-02-25 20:52:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function add() {
+    var x="20";
+    var y="40";
+    var sum = x+y;
+    document.getElementById("Demo1").innerHTML = "addition = " + sum;
+}
+function sub() {
+    var x="20";
+    var y="40";
+    var sub = x-y;
+    document.getElementById("Demo2").innerHTML = "subtraction = " + sub;
+}
+function mult() {
+    var x="20";
+    var y="40";
+    var mult = x*y;
+    document.getElementById("Demo3").innerHTML = "Product = " + mult;
+}
+function div() {
+    var x="20";
+    var y="40";
+    var div = x/y;
+    document.getElementById("Demo4").innerHTML = "Quationt = " + div;
+}
